2e043ec8f535a19de684c98d6fef5719ac595694
Jimmy Oliva <olivajames110@gmail.com>
2020-02-18 23:16:46 -0500

    home

--- src/components/screens/addGuest/addGuest.js
+++ src/components/screens/addGuest/addGuest.js
@@ -1,21 +1,11 @@
 import React, { Fragment } from "react";
-import TextFieldInput from "../../../ui-components/form_inputs/textField/textField";
-import DropDownInput from "../../../ui-components/form_inputs/dropDown/dropDown";
-import TextAreaInput from "../../../ui-components/form_inputs/textArea/textArea";
-import CheckboxInput from "../../../ui-components/form_inputs/checkbox/checkbox";
+import Form from "./form/form";
 import "./addGuest.css";
 
 const AddGuestScreen = props => {
-    let handleAddUser = () => {
-        console.log("User Added");
-    };
     return (
         <div className="add-user-container">
-            <TextFieldInput className="input-container" id="name" label="Party Name:" type="text" />
-            <DropDownInput className="input-container" id="name" label="Party Name:" type="text" />
-            <TextAreaInput className="input-container" id="name" label="Party Name:" type="text" />
-            <CheckboxInput className="input-container" id="name" label="Party Name:" type="text" />
-            <button onClick={handleAddUser}>Submit</button>
+            <Form />
         </div>
     );
 };
