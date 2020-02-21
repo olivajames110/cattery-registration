5a9fef67379f70e98c09f01e241dbc57fe663504
Jimmy Oliva <olivajames110@gmail.com>
2020-02-20 21:02:34 -0500

    wokring form update

--- src/components/screens/addUser/form/form.js
+++ src/components/screens/addUser/form/form.js
@@ -1,45 +1,62 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import axios from 'axios';
-import TextFieldInput from '../../../../ui-components/form_inputs/textField/textField';
-import DropDownInput from '../../../../ui-components/form_inputs/dropDown/dropDown';
-import TextAreaInput from '../../../../ui-components/form_inputs/textArea/textArea';
-import CheckboxInput from '../../../../ui-components/form_inputs/checkbox/checkbox';
-import DobPicker from '../../../../ui-components/form_inputs/dobPicker/dobPicker';
-import ElectronicSignature from '../../../../ui-components/signature/signature';
-import './form.css';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+import TextFieldInput from "../../../../ui-components/form_inputs/textField/textField";
+import DropDownInput from "../../../../ui-components/form_inputs/dropDown/dropDown";
+import TextAreaInput from "../../../../ui-components/form_inputs/textArea/textArea";
+import CheckboxInput from "../../../../ui-components/form_inputs/checkbox/checkbox";
+import DobPicker from "../../../../ui-components/form_inputs/dobPicker/dobPicker";
+import ElectronicSignature from "../../../../ui-components/signature/signature";
+import "./form.css";
 
-const AddUserscreen = (props) => {
-	let [ users, setUsers ] = useState([]);
+const AddUserscreen = props => {
+    let [firstName, setFirstName] = useState("");
+    let [lastName, setLastName] = useState("");
+    const [dataUrl, setDataUrl] = useState(null);
+    const user = {
+        firstName: firstName,
+        lastName: lastName,
+        dataUrl: dataUrl
+    };
+    let endPoint = "local";
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		// console.log(e);
-		const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-		// console.log(res);
-	};
-	// useEffect(() => {
-	// 	handleSubmit();
-	// }, []);
+    const handleSubmit = async e => {
+        e.preventDefault();
+        console.log(user);
+        // axios.post('http://localhost:3000/add-user' , user)
+        // const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+        // console.log(res);
+    };
 
-	return (
-		<div className="add-user-container">
-			<form onSubmit={handleSubmit}>
-				<div className="name-container">
-					<TextFieldInput id="first-name" label="First Name" name="first-name" />
-					<TextFieldInput id="last-name" label="Last Name" name="last-name" />
-				</div>
-				<DobPicker />
-				<TextFieldInput id="email-address" label="Email Address" name="email-address" />
-				<CheckboxInput
-					id="email-consent"
-					label="Check to receive information, news, and discounts by e-mail."
-					value="valueName"
-					text="Check to receive information, news, and discounts by e-mail."
-				/>
-				<ElectronicSignature />
-			</form>
-		</div>
-	);
+    // let onInputChange = e => {
+    //     setUser();
+    // };
+    let updateUrl = url => {
+        // e.preventDefault();
+        setDataUrl(url);
+        console.log("update url");
+    };
+
+    // useEffect(() => {
+    // 	handleSubmit();
+    // }, []);
+
+    return (
+        <div className="add-user-container">
+            <form onSubmit={handleSubmit}>
+                <div className="name-container">
+                    <TextFieldInput onChange={e => setFirstName(e.target.value)} id="first-name" label="First Name" name="first-name" />
+                    <TextFieldInput onChange={e => setLastName(e.target.value)} id="last-name" label="Last Name" name="last-name" />
+                </div>
+                <DobPicker />
+                <TextFieldInput id="email-address" label="Email Address" name="email-address" />
+                <CheckboxInput id="email-consent" label="Check to receive information, news, and discounts by e-mail." value="valueName" text="Check to receive information, news, and discounts by e-mail." />
+                <ElectronicSignature updateUrl={updateUrl} />
+            </form>
+            <button onClick={handleSubmit} className="submit-btn">
+                Submit
+            </button>
+        </div>
+    );
 };
 
 export default AddUserscreen;
--- src/ui-components/signature/signature.js
+++ src/ui-components/signature/signature.js
@@ -1,31 +1,49 @@
-import React, { useRef, useEffect, useState } from 'react';
-import SignatureCanvas from 'react-signature-canvas';
-import './signature.css';
+import React, { useRef, useEffect, useState } from "react";
+import SignatureCanvas from "react-signature-canvas";
+import "./signature.css";
 
-const ElectronicSignature = () => {
-	const [ containerWidth, setContainerWidth ] = useState(null);
-	const ref = useRef(null);
-	// const containerWidth = document.querySelector('.electronic-signature-container').offsetWidth;
-	useEffect(
-		() => {
-			const width = ref.current ? ref.current.offsetWidth : 0;
-			setContainerWidth(width);
-			console.log('width', width);
-		},
-		[ ref.current ]
-	);
-	return (
-		<div className="input-container">
-			<label>Signature</label>
-			<div ref={ref} className="electronic-signature-container">
-				<SignatureCanvas
-					penColor="green"
-					canvasProps={{ width: containerWidth, height: 200, className: 'sigCanvas' }}
-				/>
-				<div className="additional-text-container" />
-			</div>
-		</div>
-	);
+const ElectronicSignature = props => {
+    const [containerWidth, setContainerWidth] = useState(null);
+    const [dataUrl, setDataUrl] = useState(null);
+    const containerRef = useRef(null);
+    // const canvasRef = useRef(null);
+    // const containerWidth = document.querySelector('.electronic-signature-container').offsetWidth;
+    let sigCanvas = {};
+    useEffect(() => {
+        const width = containerRef.current ? containerRef.current.offsetWidth : 0;
+        setContainerWidth(width);
+        console.log("width", width);
+    }, [containerRef.current]);
+
+    let clear = () => {
+        console.log("clears");
+        sigCanvas.clear();
+    };
+
+    let createImage = e => {
+        e.preventDefault();
+        let url = sigCanvas.toDataURL();
+        props.updateUrl(url);
+        // setDataUrl(url);
+        // console.log(url);
+    };
+
+    return (
+        <div className="input-container">
+            <label>Signature</label>
+            <div ref={containerRef} className="electronic-signature-container">
+                <SignatureCanvas ref={ref => (sigCanvas = ref)} penColor="green" canvasProps={{ width: containerWidth, height: 200, className: "sigCanvas" }} />
+                <div className="additional-text-container">
+                    <button onClick={clear} className="clear-btn">
+                        Clear
+                    </button>
+                    <button onClick={createImage} className="clear-btn">
+                        Create
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ElectronicSignature;
